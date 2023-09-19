986b51afa786871abae9a6c09003a2ae0825f99d
Horiyormee <salaudeenhalimat320@gmail.com>
2023-09-19 07:37:25 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include "_putchar"
+
 /**
  * print_rev - imprime en reversa
  * @s: string
@@ -15,7 +17,7 @@ void print_rev(char *s)
 		s++;
 	}
 	s--;
-	for (o = longi; o > 0; o--)
+	for (o = longi; o > 0;) o--;
 	{
 		_putchar(*s);
 		s--;
